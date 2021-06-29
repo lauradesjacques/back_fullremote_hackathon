6ad2ae4f0b086ac704cc971d55db9141d41b95f5
laura desjacques <laura-desjacques_student2021@wilder.school>
2021-06-30 01:10:30 +0200

    ok

--- src/main/java/com/fullremote/fullremote/MailController.java
+++ src/main/java/com/fullremote/fullremote/MailController.java
@@ -4,10 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin
 @RestController
 
 public class MailController {
